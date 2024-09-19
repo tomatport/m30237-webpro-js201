f73b058c2e02bd645274c52ebc78ca7b704cf9a5
Thomas Robinson <git@tomr.me>
2024-09-19 23:17:18 +0100

    all green! 💪

--- index.js
+++ index.js
@@ -35,3 +35,90 @@ function subtract(a, b) {
 function checkObject(obj) {
   obj["checked"] = true;
 }
+
+function checkObjectInside(obj) {
+  if ("data" in obj) {
+    obj["data"]["checked"] = true;
+  }
+}
+
+function arraySet(arr, i, n) {
+  if(arr[i]) arr[i] = n;
+}
+
+function addAll(arr) {
+  let sum = 0;
+  arr.forEach(num => sum += num);
+  return sum;
+}
+
+function larger(a, b) {
+  return (a > b) ? a : b;
+}
+
+function largest(arr) {
+  let max; // we can't set this to 0, since we might have all negative #s
+  arr.forEach(num => {
+    if(num > max || !max) max = num;
+  });
+  return max ? max : null;
+}
+
+function compare(a, b) {  
+  if(a.length !== b.length) return false;
+
+  for(const i in a) {
+    if(a[i] !== b[i]) return false;
+  }
+
+  return true;
+}
+
+function addToAll(arr, n) {
+  for(const i in arr) {
+    arr[i] += n;
+  }
+  return arr;
+}
+
+let remembered = undefined;
+function rememberThis(keepsake) {
+  remembered = keepsake;
+}
+
+function nArray(n) {
+  let arr = [];
+
+  for (let i = 0; i < n; i++) {
+    arr[i] = i + 1;
+  }
+
+  return arr;
+}
+
+function addAllOpt(arr) {
+  if(!arr || arr.length === 0) return 0;
+
+  return addAll(arr);
+}
+
+function divisors(arr, div) {
+  let divisable = [];
+
+  arr.forEach(n => {
+    if(n % div === 0) divisable.push(n);
+  });
+
+  return divisable;
+}
+
+function multiples(n, m) {
+  let arr = [];
+
+  // similar to nArray!
+  for (let i = 0; i < n; i++) {
+    arr[i] = (i + 1) * m;
+  }
+
+  return arr;
+}
